43b6d11cb7d736525ae4efc6fe28bda1e45135e4
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-06 01:55:25 +0500

    created new c# proj with arithmetic operations

---
+++ lesson001/task02/Program.cs
@@ -0,0 +1,20 @@
+﻿int i1 = 5;
+int i2 = 3;
+long l1 = 4;
+double d1 = 2.3;
+
+int sum1 = i1 + i2;
+long sum2 = l1 + i1;
+double sum3 = d1 + i2;
+
+int prod1 = i1 * i2;
+long prod2 = l1 * i1;
+double prod3 = d1 * i2;
+
+Console.WriteLine(sum1);
+Console.WriteLine(sum2);
+Console.WriteLine(sum3);
+
+Console.WriteLine(prod1);
+Console.WriteLine(prod2);
+Console.WriteLine(prod3);

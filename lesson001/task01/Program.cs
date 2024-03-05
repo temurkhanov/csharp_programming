41890b66b43160e8d0fffe4b0fd5c4096d5ef736
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-06 01:37:41 +0500

    initialized variables with several data types

--- lesson001/task01/Program.cs
+++ lesson001/task01/Program.cs
@@ -1,2 +1,13 @@
 ﻿
 Console.WriteLine("Hello, World!");
+
+int val = 100;
+Console.WriteLine(val);
+
+double d = 5.21;
+bool b = true;
+char c = '!';
+
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);

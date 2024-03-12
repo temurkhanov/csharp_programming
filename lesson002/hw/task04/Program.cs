5ef72e943aa79275d207ef0fa1aca491672f2e37
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-13 00:48:16 +0500

    solved 4th hw task

---
+++ lesson002/hw/task04/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите натуральное число");
+n_input:
+int n = Convert.ToInt32(Console.ReadLine());
+if (n<0)
+{
+    System.Console.WriteLine("Число не натуральное. Повторите ввод:");
+    goto n_input;
+}
+while (n>0)
+{
+    int res = n%10;
+    n=n/10;
+    System.Console.WriteLine(res + ",");
+}

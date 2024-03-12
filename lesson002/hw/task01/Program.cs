7e2bd4d9dcf6a86ba8a66a7399fb5906981f0fd1
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-12 23:57:31 +0500

    solved first hw task

---
+++ lesson002/hw/task01/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Напишите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Кратность 7: ");
+if (num%7 == 0)
+{
+    System.Console.WriteLine("+");
+}
+else
+{
+    System.Console.WriteLine("-");
+}
+System.Console.Write("Кратность 23: ");
+if (num%23 == 0)
+{
+    System.Console.WriteLine("+");
+}
+else
+{
+    System.Console.WriteLine("-");
+}

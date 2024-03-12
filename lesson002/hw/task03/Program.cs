26e7d1dc1419586193e8f071a9f829b9e1d7f82a
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-13 00:31:04 +0500

    solved 3rd hw task

---
+++ lesson002/hw/task03/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.WriteLine("Напишите десятичное число:");
+num_input:
+int num = Convert.ToInt32(Console.ReadLine());
+if (num<10 || num>99)
+{
+    System.Console.WriteLine("Число не десятичное. Повторите ввод:");
+    goto num_input;
+}
+int num1 = num/10;
+int num2 = num%10;
+if (num1>=num2)
+{
+    if (num1==num2)
+    {
+        System.Console.WriteLine($"Цифры равны: {num1}");
+    }
+    System.Console.WriteLine(num1);
+}
+else
+{
+    System.Console.WriteLine(num2);
+}

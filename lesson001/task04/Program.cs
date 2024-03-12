20594dbd05a93926b15b5eb6f5cba4e6fe100014
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-12 15:41:21 +0500

    added task04

---
+++ lesson001/task04/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 0;
+int b = 4;
+int c = 5;
+int d = 23;
+int e = 9;
+
+int max = a;
+
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+if (max < d)
+{
+    max = d;
+}
+if (max < e)
+{
+    max = e;
+}
+Console.WriteLine(max);

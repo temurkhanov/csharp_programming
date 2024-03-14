8f2bedb82c5c43213ce0df5b7a434f5583aacc0e
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-14 21:02:40 +0500

    lesson2 array first task

---
+++ lesson002/task01/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arr = new int [5];
+arr[0]=1;
+arr[1]=4;
+arr[2]=7;
+arr[3]=5;
+arr[4]=9;
+
+for(int i=0; i<5; i++)
+{
+    Console.WriteLine(arr[i]);
+}
+
+int[] arr2= new int[5] {1,2,3,4,5};
+int[] arr3 = {6,7,8,9,0};

f98c5aa1f22bd2fd1ed36d564586a2e4530a7629
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-16 16:28:33 +0500

    les2 task4: max num sorting code added

---
+++ lesson002/task04/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] weight = { 5, 8, 7, 6, 9, 1, 3, 5, 6, 4, 8, 11, 15, 4, 3, 7, 14};
+int max=0;
+
+for (int i = 0; i < weight.Length; i++)
+{
+    if (weight[i] > max)
+    {
+        max = weight[i];
+    }
+    //Console.WriteLine("one cycle");
+}
+
+Console.Write(max);

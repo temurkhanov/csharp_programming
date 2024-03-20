44622697739a9c2afbe336a9363eb57ca1c8d2a8
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-20 18:03:19 +0500

    les3 task2 void function

---
+++ lesson003/task02/Program.cs
@@ -0,0 +1,11 @@
+﻿void PrintSquares(int n)
+{
+    for (int i=0; i<n; i++)
+    {
+        Console.Write(i*i + " ");
+    }
+    Console.WriteLine();
+}
+PrintSquares(new Random().Next(1,10));
+PrintSquares(new Random().Next(1,10));
+PrintSquares(new Random().Next(1,10));

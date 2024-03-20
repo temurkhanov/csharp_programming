bef0eedc3402e640ccc0c2e980d4065a343aeaf8
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-20 17:54:14 +0500

    first function code, les3, task1

---
+++ lesson003/task01/Program.cs
@@ -0,0 +1,8 @@
+﻿double CalculateFor(int a, int b, int c, int d)
+{
+    int numerator = a*b;
+    double denominator =c+d;
+    double result = numerator/denominator;
+    return result;
+}
+Console.Write(CalculateFor(new Random().Next(1,10),new Random().Next(1,10),new Random().Next(1,10),new Random().Next(1,10)));

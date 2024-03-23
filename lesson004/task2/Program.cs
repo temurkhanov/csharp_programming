3c6a7d60cb2c0ca17da94129d2b2b68005de5c43
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-23 20:13:47 +0500

    les4 task2 even num calc code

---
+++ lesson004/task2/Program.cs
@@ -0,0 +1,51 @@
+﻿int[,] fill_matrix(int row, int col)
+{
+    int[,] some_matrix= new int[row, col];
+    for (int i=0; i<row; i++)
+    {
+        for (int j=0;j<col;j++)
+        {
+            some_matrix[i,j]=new Random().Next(0,9);
+        }
+    }
+    return some_matrix;
+}
+void print_matrix(int[,] print_matrix)
+{
+    for (int i=0;i<print_matrix.GetLength(0);i++)
+    {
+        for (int j=0; j<print_matrix.GetLength(1);j++)
+        {
+            Console.Write($"{print_matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    
+}
+bool IsInteresting(int num)
+{
+    bool IsInteresting=false;
+    if (num%2==0)
+    {
+        IsInteresting=true;
+    }
+    return IsInteresting;
+}
+int InterestingNumCount(int [,] check_matrix)
+{
+    int count=0;
+    for (int i=0; i<check_matrix.GetLength(0); i++)
+    {
+        for (int j=0;j<check_matrix.GetLength(1);j++)
+        {
+            if (IsInteresting(check_matrix[i,j]))
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+int[,] sample_matrix=fill_matrix(4,5);
+print_matrix(sample_matrix);
+Console.Write(InterestingNumCount(sample_matrix));

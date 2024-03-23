8d337d3bfcb01a55f961a932241dde0e5f56d490
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-23 17:30:39 +0500

    les4 task1 first matrix code

---
+++ lesson004/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿int[,] fill_matrix(int row, int col)
+{
+    int[,] some_matrix= new int[row, col];
+    for (int i=0; i<row; i++)
+    {
+        for (int j=0;j<col;j++)
+        {
+            some_matrix[i,j]=new Random().Next(0,9);
+        }
+    }
+    return some_matrix;
+}
+void print_matrix(int[,] print_matrix)
+{
+    for (int i=0;i<print_matrix.GetLength(0);i++)
+    {
+        for (int j=0; j<print_matrix.GetLength(1);j++)
+        {
+            Console.Write($"{print_matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    
+}
+int[,] sample_matrix=fill_matrix(4,5);
+print_matrix(sample_matrix);

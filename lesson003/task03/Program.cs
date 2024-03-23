15c6ec969e66e04cb0713b2495b27b09ff8f5c71
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-23 17:18:31 +0500

    les3, task3 changes

---
+++ lesson003/task03/Program.cs
@@ -0,0 +1,16 @@
+﻿void ZeroEvenEl(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+int[] a = { 1, 2, 3, 4, 5, 6, 7, 8 };
+ZeroEvenEl(a);
+for (int i=0;i<a.Length;i++)
+{
+    Console.Write(a[i] + " ");
+}

ca1c0bd41a4d26a2145f42b9984f00f2364f28e5
Komilkhon <temurkhanov.akfa@gmail.com>
2024-03-29 02:11:59 +0500

    les5 lection task

---
+++ lesson005/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int FindFactorialOfN(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    return n *= FindFactorialOfN(n - 1);
+}
+System.Console.WriteLine("Please enter integer number:");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(FindFactorialOfN(number));
